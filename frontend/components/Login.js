05be36a5d28b4f59ca8b07366442528f16561860
Guillaume MEYER <guimeyer64@gmail.com>
2025-07-17 14:41:16 +0200

    auth en cours

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,18 +1,49 @@
+import { useState } from 'react';
 import styles from '../styles/Login.module.css';
+import { Modal } from 'antd';
+import SignUp from './SignUp';
 
 function Login() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+        console.log('click')
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div className={styles.container}>
-            <img className={styles.image} src='/imageAccueil.png' alt='imageAccueil'/>
+            <img className={styles.image} src='/imageAccueil.png' alt='imageAccueil' />
             <div className={styles.textContainer}>
-                <img className={styles.logo} src='/logofondnoir.png' alt='imageAccueil'/>
-                <h1 className={styles.h1}>See what's <br></br>happening</h1>
+                <img className={styles.logo} src='/logofondnoir.png' alt='imageAccueil' />
+                <h1 className={styles.h1}>See what's <br /> happening</h1>
                 <h2 className={styles.h2}>Join Hackatweet today.</h2>
-                <button className={styles.signup}>Sign up</button>
+                
+                <button className={styles.signup} onClick={showModal}>Sign up</button>
+                
                 <p className={styles.p}>Already have an account?</p>
-                <button className={styles.signin}>Sign in</button>                
+                <button className={styles.signin}>Sign in</button>
             </div>
-            
+
+            <Modal
+                title={null}
+                visible={isModalOpen} 
+                onOk={handleOk}
+                onCancel={handleCancel}
+                closable={true}
+                footer={null}
+                wrapClassName={styles.customModal}
+            >
+                <SignUp></SignUp>
+            </Modal>
 
         </div>
     );
